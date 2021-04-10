29ee88c179e792f863ecabd6fb1632b1149b8d6a
SarasovMatvey <sarasovmatvej@gmail.com>
2021-04-10 14:22:33 +0600

    added more animation

--- src/js/_blog-info.js
+++ src/js/_blog-info.js
@@ -5,9 +5,10 @@
       type: "bullets",
       bulletClass: "section-slider__pagination-bullet",
       bulletActiveClass: "section-slider__pagination-bullet_active",
+      clickable: true,
     },
     autoplay: {
-      delay: 5000,
+      delay: 2000,
       disableOnInteraction: false,
     },
   });
